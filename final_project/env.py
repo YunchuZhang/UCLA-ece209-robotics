69fe7accb86e976dd76c7ba59f9d373b855f81d3
Yunchu <33641269+YunchuZhang@users.noreply.github.com>
2018-11-27 02:52:36 -0800

    Add files via upload

--- final_project/env.py
+++ final_project/env.py
@@ -25,30 +25,30 @@ class Viewer(pyglet.window.Window):
 
     def __init__(self):
         # vsync=False to not use the monitor FPS, we can speed up training
-        super(Viewer, self).__init__(width=400, height=400, resizable=False, caption='Arm', vsync=False)
+        super(Viewer, self).__init__(width=800, height=500, resizable=False, caption='Env', vsync=False)
         pyglet.gl.glClearColor(1, 1, 1, 1)
 
         self.batch = pyglet.graphics.Batch()    # display whole batch at once
         self.point = self.batch.add(
             4, pyglet.gl.GL_QUADS, None,    # 4 corners
             ('v2f', [50, 50,                # location
-                     50, 100,
-                     100, 100,
+                     50, 500,
+                     100, 500,
                      100, 50]),
             ('c3B', (86, 109, 249) * 4))    # color
-        self.arm1 = self.batch.add(
+        self.obstacle1 = self.batch.add(
             4, pyglet.gl.GL_QUADS, None,
-            ('v2f', [250, 250,                # location
-                     250, 300,
-                     260, 300,
-                     260, 250]),
+            ('v2f', [600, 150,                # location
+                     600, 400,
+                     610, 400,
+                     610, 150]),
             ('c3B', (249, 86, 86) * 4,))    # color
-        self.arm2 = self.batch.add(
+        self.obstacle1 = self.batch.add(
             4, pyglet.gl.GL_QUADS, None,
-            ('v2f', [100, 150,              # location
-                     100, 160,
+            ('v2f', [200, 150,              # location
                      200, 160,
-                     200, 150]), ('c3B', (249, 86, 86) * 4,))
+                     500, 160,
+                     500, 150]), ('c3B', (249, 86, 86) * 4,))
 
     def render(self):
         self._update_arm()
